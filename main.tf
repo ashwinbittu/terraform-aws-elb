565e6af87718101411dd34b2de4928a746fe553a
ashwinbittu <ashwin.bittu@gmail.com>
2020-08-17 17:26:15 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,21 @@ resource "aws_elb" "my-elb" {
     lb_port           = 80
     lb_protocol       = "http"
   }
+  listener {
+    instance_port      = 80
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = var.lb_ssl_id
+  }  
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
     target              = "HTTP:80/"
-    interval            = 30
+    interval            = 10
   }
-
+  
   cross_zone_load_balancing   = true
   connection_draining         = true
   connection_draining_timeout = 400
